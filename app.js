e8d3ac0e15a88a1c16370b4a98f06355da9818a3
Vitalii Otiuskyi <vitalii.otiuskyi@gmail.com>
2023-07-08 17:15:04 +0300

    added slider

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ function onClick() {
 let likeBtns = document.querySelectorAll(".like");
 
 function imageLikeBtn() {
-//   console.log("click");
+  //   console.log("click");
   let imgSrc = this.getAttribute("src");
 
-  if (imgSrc === "img/icon-5.png" || this.style.backgroundColor === "transparent") {
+  if (
+    imgSrc === "img/icon-5.png" ||
+    this.style.backgroundColor === "transparent"
+  ) {
     this.setAttribute("src", "img/icon-7.png");
     this.style.backgroundColor = "#2c71b8";
   } else {
@@ -21,5 +24,35 @@ function imageLikeBtn() {
     this.style.backgroundColor = "transparent";
   }
 }
-
 likeBtns.forEach((likeBtn) => likeBtn.addEventListener("click", imageLikeBtn));
+
+//------------------------FORM----------------------------
+let moreBtn = document.querySelectorAll(".item-more-btn");
+let modal = document.querySelector(".modal");
+let formBtn = document.querySelector(".btn-close-modal");
+
+function openModal() {
+  modal.classList.remove("hide");
+  modal.classList.add("show");
+}
+
+function closeModal() {
+  modal.classList.remove("show");
+  modal.classList.add("hide");
+}
+
+moreBtn.forEach((btnMore) => btnMore.addEventListener("click", openModal));
+formBtn.addEventListener("click", closeModal);
+
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
+
+//------------------------SLIDER----------------------------
+$(".slider").slick({
+    dots: true,
+    prevArrow: '.prev-arrow',
+    nextArrow: '.next-arrow',
+});
